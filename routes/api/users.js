e125e02194d59761b7787d9fe521682a2b4e34ae
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-08-18 15:13:38 +0200

    Add Passport JWT Authentication Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keyes = require('../../config/keys');
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
@@ -92,6 +93,19 @@ router.post('/login', (req, res) => {
     })
 });
 
+// @route   GET api/users/current
+// @desc    Current User / Returning the current user
+// @access  Private
 
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email,
+        avatar: req.user.avatar,
+        date: req.user.date
 
+    });
+});
+ 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -25,9 +26,16 @@ mongoose
     .then(() => console.log('MongoDb Connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-});
+
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport Config
+require('./config/passport')(passport);
+
+
 
 // Use routes
 app.use('/api/users', users);
